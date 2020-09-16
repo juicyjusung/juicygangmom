136258239cb083fc2d0aa83c360f34f6a1bbb56e
juicyjusung <juicyjusung@gmail.com>
2020-09-16 23:09:15 +0900

    chore: Add scafold vue file

---
+++ components/Scafold.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Scafold extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
