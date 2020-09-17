38055bfc48c1d7bc178aaecc93039720eb78b4c5
juicyjusung <juicyjusung@gmail.com>
2020-09-17 23:00:41 +0900

    feat: 리뷰 아이템 리스트 컴포넌트 추가

---
+++ components/review/ReviewList.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="reviews__wrapper">
+    <!--  for mobile-->
+    <div v-if="isMobile" class="reviews__card">
+      <Carousel class="review_carousel" :per-page="1">
+        <slide v-for="(item, index) in reviews" :key="index">
+          <ReviewItem :review="item" :is-show="isLoggined || index === 0"></ReviewItem>
+        </slide>
+      </Carousel>
+    </div>
+
+    <!--  for PC-->
+    <div v-if="!isMobile">
+      <!-- for loggined user-->
+      <div v-if="isLoggined">
+        <div class="reviews__card">
+          <div v-for="(item, index) in reviews" :key="index + 'pc'" class="reviews__item">
+            <ReviewItem
+              :review="item"
+              :is-show="isLoggined"
+              class="reviews__item"
+            ></ReviewItem>
+          </div>
+        </div>
+      </div>
+      <!-- for not loggined user-->
+      <div v-else>
+        <ReviewItem
+          :review="reviews[0]"
+          :is-show="true"
+          class="reviews__card"
+        ></ReviewItem>
+        <div class="reviews__card">
+          <div
+            v-for="(item, index) in reviews.slice(1, reviews.length)"
+            :key="index + 'pc'"
+          >
+            <ReviewItem
+              :review="item"
+              :is-show="isLoggined"
+              class="reviews__item"
+            ></ReviewItem>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+// @ts-ignore
+import { Carousel, Slide } from 'vue-carousel';
+import PageBase from '~/pages/PageBase';
+import { Review } from '~/types/Review';
+import ReviewItem from '~/components/review/ReviewItem.vue';
+
+@Component({
+  components: {
+    ReviewItem,
+    Carousel,
+    Slide,
+  },
+})
+export default class ReviewList extends PageBase {
+  /******************************************************************
+   * Props & Emit
+   * ****************************************************************/
+  @Prop(Array) readonly reviews!: Review[];
+
+  /******************************************************************
+   * Variable
+   * ****************************************************************/
+  private isLoggined = true;
+  /******************************************************************
+   * Life Cycles
+   * ****************************************************************/
+
+  private async created() {}
+
+  /******************************************************************
+   * Methods
+   * ****************************************************************/
+}
+</script>
+
+<style lang="scss" scoped>
+.reviews__card {
+  margin: 8px 0;
+  background: #ffffff;
+  border-radius: 20px;
+}
+.reviews__item {
+  &:not(:first-child) {
+    border-top: 1px solid rgba(0, 0, 0, 0.05);
+  }
+}
+</style>
+
+<style lang="scss">
+.VueCarousel-dot-container {
+  margin-top: 0 !important;
+}
+</style>
