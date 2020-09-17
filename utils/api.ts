35177d793bbb0cada7446b5e593d8e277397cad1
juicyjusung <juicyjusung@gmail.com>
2020-09-17 23:50:22 +0900

    feat: Customize axios instance

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,32 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
+import { eventHub } from '~/utils/evnetHub';
 
 // eslint-disable-next-line
 let $axios: NuxtAxiosInstance;
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance): void {
   $axios = axiosInstance;
+  $axios.interceptors.request.use(
+    conf => {
+      eventHub.$emit('before-request');
+      return conf;
+    },
+    error => {
+      eventHub.$emit('request-error');
+      return Promise.reject(error);
+    }
+  );
+
+  $axios.interceptors.response.use(
+    response => {
+      eventHub.$emit('after-response');
+      return response;
+    },
+    error => {
+      eventHub.$emit('response-error');
+      return Promise.reject(error);
+    }
+  );
 }
 
 export { $axios };
