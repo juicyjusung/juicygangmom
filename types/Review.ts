09e9475a789c46f24bffc0738fc3f6e8a8e91677
juicyjusung <juicyjusung@gmail.com>
2020-09-17 22:55:56 +0900

    feat: 타입 정의

---
+++ types/Review.ts
@@ -0,0 +1,37 @@
+export interface Review {
+  id: number;
+  title: string;
+  academyId: number;
+  academyName: string;
+  academyLogo: string;
+  startGrade: string;
+  subject: string;
+  courseTerm: string;
+  createdAt: string;
+  className: string;
+  teacherName: string;
+  pros: string;
+  cons: string;
+  rating: number;
+  writer: string;
+  recommended: boolean;
+}
+
+// export interface Review {
+//   id: number;
+//   title: string;
+//   academy_id: number;
+//   academy_name: string;
+//   academy_logo: string;
+//   start_grade: string;
+//   subject: string;
+//   course_term: string;
+//   created_at: string;
+//   class_name: string;
+//   teacher_name: string;
+//   pros: string;
+//   cons: string;
+//   rating: number;
+//   writer: string;
+//   recommended: boolean;
+// }
---
+++ types/Statistics.ts
@@ -0,0 +1,5 @@
+export interface Statistics {
+  recommendedRate: number;
+  starAvg: number;
+  starRates: object;
+}
