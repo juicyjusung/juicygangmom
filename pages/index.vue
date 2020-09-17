eb924945d88c9d67f18aead9398f4e2622bd254c
juicyjusung <juicyjusung@gmail.com>
2020-09-17 23:02:10 +0900

    feat: 인덱스 페이지 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,139 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">juicygangmom</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="grid">
+    <div class="reviews__container">
+      <h1 class="reviews__heading">
+        리뷰<span class="heading__count">총{{ totalElements }}개</span>
+      </h1>
+      <Statistic :statistics="statistics"></Statistic>
+      <ReviewList :reviews="reviews"></ReviewList>
+      <div class="reviews__btn-wrapper">
+        <a class="reviews__btn reviews__btn--red"><i class="icon-write" />리뷰쓰기</a>
+        <a class="reviews__btn reviews__btn--gray">리뷰{{ totalElements }}개 전체보기</a>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component } from 'vue-property-decorator';
+import { Action } from 'vuex-class';
+import Statistic from '~/components/review/Statistic.vue';
+import ReviewList from '~/components/review/ReviewList.vue';
+import PageBase from '~/pages/PageBase';
+import { Review } from '~/types/Review';
 
-export default Vue.extend({
-  components: { }
+@Component({
+  components: { ReviewList, Statistic },
 })
+export default class Index extends PageBase {
+  /******************************************************************
+   * Store
+   * ****************************************************************/
+  @Action('getReviews', { namespace: 'review' })
+  private getReviews!: (academyId: number) => any;
+
+  @Action('getStatistics', { namespace: 'review' })
+  private getStatistics!: (academyId: number) => Statistic;
+  /******************************************************************
+   * Props & Emit
+   * ****************************************************************/
+
+  /******************************************************************
+   * Variable
+   * ****************************************************************/
+  private reviews: Review[] = [];
+  private totalElements: number = 0;
+  private statistics: Statistic | null = null;
+
+  /******************************************************************
+   * Life Cycles
+   * ****************************************************************/
+
+  private async created() {
+    await this.fetchData();
+  }
+
+  /******************************************************************
+   * Methods
+   * ****************************************************************/
+  private async fetchData() {
+    const [reviewsData, statisticsData] = await Promise.all([
+      this.getReviews(1),
+      this.getStatistics(1),
+    ]);
+    this.totalElements = reviewsData.totalElements;
+    this.reviews = reviewsData.content as Review[];
+    this.statistics = statisticsData;
+  }
+}
 </script>
 
-<style>
-.container {
+<style lang="scss">
+@import '@/assets/styles/mixin';
+
+.grid {
   margin: 0 auto;
-  min-height: 100vh;
+  width: 100%;
+  display: grid;
+  max-width: 1040px;
+  grid-template-columns: 0 repeat(4, 1fr) 0;
+  column-gap: 8px;
+  @include desktop {
+    grid-template-columns: 0 repeat(12, 1fr) 0;
+    column-gap: 4px;
+  }
+}
+.reviews__container {
+  grid-column: 2 / span 4;
+  @include desktop {
+    grid-column: 2 / span 12;
+  }
+}
+
+.reviews__heading {
   display: flex;
-  justify-content: center;
+  flex-direction: row;
   align-items: center;
+  font-weight: 800;
+  line-height: 150%;
+  color: #444951;
+  font-size: 24px;
+  margin-bottom: 10px;
+  @include desktop {
+    font-size: 32px;
+  }
+  .heading__count {
+    font-weight: 800;
+    line-height: 150%;
+    color: #a0a5ae;
+    margin-left: 8px;
+    font-size: 16px;
+    @include desktop {
+    }
+  }
+}
+.reviews__btn-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.reviews__btn {
+  cursor: pointer;
+  font-size: 12px;
+  padding: 23px 0;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  border-radius: 20px;
+  margin: 5px 0;
+  @include desktop {
+    font-size: 16px;
+  }
+  &--red {
+    color: #ffffff;
+    background: #ff583f;
+  }
+  &--gray {
+    color: #727884;
+    background: #e5e6ec;
+  }
 }
 </style>
